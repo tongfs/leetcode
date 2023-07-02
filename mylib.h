0754367d9f60b0f56458f402faed99f30a5ae2fa
tongfs <tongfs@stu.pku.edu.cn>
2023-07-02 17:01:10 +0800

    init

---
+++ mylib.h
@@ -0,0 +1,62 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+// 链表结构
+struct ListNode {
+    int val;
+    ListNode *next;
+    ListNode() : val(0), next(nullptr) {}
+    ListNode(int x) : val(x), next(nullptr) {}
+    ListNode(int x, ListNode *next) : val(x), next(next) {}
+};
+
+// 二叉树结构
+struct TreeNode {
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+    TreeNode() : val(0), left(nullptr), right(nullptr) {}
+    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+    TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+};
+
+// 带next指针的二叉树
+class Node {
+public:
+    int val;
+    Node* left;
+    Node* right;
+    Node* next;
+
+    Node() : val(0), left(NULL), right(NULL), next(NULL) {}
+    Node(int _val) : val(_val), left(NULL), right(NULL), next(NULL) {}
+    Node(int _val, Node* _left, Node* _right, Node* _next)
+        : val(_val), left(_left), right(_right), next(_next) {}
+};
+
+// nested list
+class NestedInteger {
+    public:
+        // Constructor initializes an empty nested list.
+        NestedInteger();
+
+        // Constructor initializes a single integer.
+        NestedInteger(int value);
+
+        // Return true if this NestedInteger holds a single integer, rather than a nested list.
+        bool isInteger() const;
+
+        // Return the single integer that this NestedInteger holds, if it holds a single integer
+        // The result is undefined if this NestedInteger holds a nested list
+        int getInteger() const;
+
+        // Return the nested list that this NestedInteger holds, if it holds a nested list
+        // The result is undefined if this NestedInteger holds a single integer
+        const vector<NestedInteger> &getList() const;
+
+        // Set this NestedInteger to hold a single integer.
+        void setInteger(int value);
+
+        // Set this NestedInteger to hold a nested list and adds a nested integer to it.
+        void add(const NestedInteger &ni);
+ };
