99c1dcfbfba092d24e625bdb2440b78beb1338cd
tongfs <tongfs@stu.pku.edu.cn>
2023-07-06 00:27:11 +0800

    [daily] 2178

---
+++ 2178.拆分成最多数目的正偶数之和.cpp
@@ -0,0 +1,40 @@
+/*
+ * @lc app=leetcode.cn id=2178 lang=cpp
+ * @lcpr version=21909
+ *
+ * [2178] 拆分成最多数目的正偶数之和
+ */
+
+#include "mylib.h"
+
+// @lc code=start
+class Solution {
+public:
+    vector<long long> maximumEvenSplit(long long finalSum) {
+        if (finalSum & 1) return {};
+
+        vector<long long> res;
+        for (int i = 2; i <= finalSum; i += 2) {
+            res.push_back(i);
+            finalSum -= i;
+        }
+        res.back() += finalSum;
+        return res;
+    }
+};
+// @lc code=end
+
+/*
+// @lcpr case=start
+// 12\n
+// @lcpr case=end
+
+// @lcpr case=start
+// 7\n
+// @lcpr case=end
+
+// @lcpr case=start
+// 28\n
+// @lcpr case=end
+
+ */
