7611f1018ff827a3dbdd77acee850ea43fae00ec
tongfs <tongfs@stu.pku.edu.cn>
2023-07-13 22:52:17 +0800

    [lib] formation and new declaration

--- mylib.h
+++ mylib.h
@@ -17,46 +17,56 @@ struct TreeNode {
     TreeNode *right;
     TreeNode() : val(0), left(nullptr), right(nullptr) {}
     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
-    TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+    TreeNode(int x, TreeNode *left, TreeNode *right)
+        : val(x), left(left), right(right) {}
 };
 
 // 带next指针的二叉树
 class Node {
 public:
     int val;
-    Node* left;
-    Node* right;
-    Node* next;
+    Node *left;
+    Node *right;
+    Node *next;
 
     Node() : val(0), left(NULL), right(NULL), next(NULL) {}
     Node(int _val) : val(_val), left(NULL), right(NULL), next(NULL) {}
-    Node(int _val, Node* _left, Node* _right, Node* _next)
+    Node(int _val, Node *_left, Node *_right, Node *_next)
         : val(_val), left(_left), right(_right), next(_next) {}
 };
 
 // nested list
 class NestedInteger {
-    public:
-        // Constructor initializes an empty nested list.
-        NestedInteger();
+public:
+    // Constructor initializes an empty nested list.
+    NestedInteger();
 
-        // Constructor initializes a single integer.
-        NestedInteger(int value);
+    // Constructor initializes a single integer.
+    NestedInteger(int value);
 
-        // Return true if this NestedInteger holds a single integer, rather than a nested list.
-        bool isInteger() const;
+    // Return true if this NestedInteger holds a single integer, rather than a
+    // nested list.
+    bool isInteger() const;
 
-        // Return the single integer that this NestedInteger holds, if it holds a single integer
-        // The result is undefined if this NestedInteger holds a nested list
-        int getInteger() const;
+    // Return the single integer that this NestedInteger holds, if it holds a
+    // single integer The result is undefined if this NestedInteger holds a
+    // nested list
+    int getInteger() const;
 
-        // Return the nested list that this NestedInteger holds, if it holds a nested list
-        // The result is undefined if this NestedInteger holds a single integer
-        const vector<NestedInteger> &getList() const;
+    // Return the nested list that this NestedInteger holds, if it holds a
+    // nested list The result is undefined if this NestedInteger holds a single
+    // integer
+    const vector<NestedInteger> &getList() const;
 
-        // Set this NestedInteger to hold a single integer.
-        void setInteger(int value);
+    // Set this NestedInteger to hold a single integer.
+    void setInteger(int value);
 
-        // Set this NestedInteger to hold a nested list and adds a nested integer to it.
-        void add(const NestedInteger &ni);
- };
+    // Set this NestedInteger to hold a nested list and adds a nested integer to
+    // it.
+    void add(const NestedInteger &ni);
+};
+
+// 278.第一个错误的版本
+bool isBadVersion(int version);
+// 374.猜数字大小
+int guess(int num);
